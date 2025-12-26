834353d1f115b6c532b79206dbf98b7bbbd19ef7
Joshua Stenger <JoshuaStenger12@outlook.com>
2025-12-26 14:58:13 -0500

    init

---
+++ pizza/src/main/java/com/pghpizzas/pizza/PghpizzaApplication.java
@@ -0,0 +1,13 @@
+package com.pghpizzas.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PghpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PghpizzaApplication.class, args);
+	}
+
+}
---
+++ pizza/src/test/java/com/pghpizzas/pizza/PghpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pghpizzas.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PghpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
